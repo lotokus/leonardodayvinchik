256dbe8a55e75287e60985aee9614442d86045b1
lotokus <107996963+lotokus@users.noreply.github.com>
2022-06-22 15:06:35 +0400

    Add files via upload

---
+++ assets/js/my.js
@@ -0,0 +1,4 @@
+document.querySelector('.defb').onmouseover = function() {
+   document.querySelector('.defr').classList.add('huila')
+   document.querySelector('.defl').style.backgroundColor = 'rgb(255, 131, 15)'
+}
